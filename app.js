adb24d506e52557beefd5479ae82b0becb9895bc
Jason Lee <flames25g@gmail.com>
2023-02-11 21:56:51 +1100

    app.js update

--- app.js
+++ app.js
@@ -8,5 +8,16 @@
 
 
 setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HoursFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >= 12 ? 'PM' : 'AM'
+    
+    timeEl
+
     
 }, 1000);
