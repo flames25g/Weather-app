c670adac619a6fd4931e637887597757585b18ef
Jason Lee <flames25g@gmail.com>
2023-02-11 22:09:37 +1100

    formatted

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-
-
-
-
-
-
-
-
-
 setInterval(() => {
-    const time = new Date();
-    const month = time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const hoursIn12HoursFormat = hour >= 13 ? hour %12: hour
-    const minutes = time.getMinutes();
-    const ampm = hour >= 12 ? 'PM' : 'AM'
-    
-    timeEl
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HoursFormat = hour >= 13 ? hour % 12 : hour;
+  const minutes = time.getMinutes();
+  const ampm = hour >= 12 ? "PM" : "AM";
 
-    
+  timeEl;
 }, 1000);
